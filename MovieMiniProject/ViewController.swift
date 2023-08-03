20b6c84950f18d0fc6490ea3cc91ce69dc94c21d
michaeliskndr <michaeliskndr@gmail.com>
2023-08-03 23:31:33 +0700

    Initial commit
    Initial Commit

---
+++ MovieMiniProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieMiniProject
+//
+//  Created by Michael Iskandar on 03/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
